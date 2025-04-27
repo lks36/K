b5f89fd6788b0fcbde7f59b1f92004bd0babdff1
kun <Kun.Li@etu.sorbonne-universite.fr>
2025-04-27 21:20:21 +0200

    old version

--- main.py
+++ main.py
@@ -5,20 +5,13 @@ import os
 import sqlite3
 from kfonctions import *
 import profil
-from partie import *
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 UPLOAD_FOLDER = "./static/avatars"
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-global lastid
-lastid = 10000
-TestRoom = Partie(10000, 0, 6)
-rooms = {}
-rooms[10000] = TestRoom 
-
-liste_themes = ["Les pièces de théâtre sont-elles meilleures que les romans ?", "L’art peut-il être séparé de l’artiste ?", "L’art doit-il être financé par des fonds publics ?", "Faut-il augmenter le salaire minimum ?"]
+rooms = {307:{"members":0, "messages":[]}, 302:{"members":0, "messages":[]}}
 
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 10 * 1000 * 1000
@@ -35,7 +28,8 @@ app.secret_key = key
 @app.route("/")
 def index():
     lb = get_leaderboard()
-    if session.get("id"):
+    print(session)
+    if session and session["id"]:
         return render_template("./index.html", leaderboard=lb, iter_liste=range(len(lb)), avatar=get_user_avatar(session["id"]))  
     else:
         return render_template("./index.html", leaderboard=lb, iter_liste=range(len(lb)), avatar="default")
@@ -68,7 +62,7 @@ def leaderboard():
 @app.route("/disconnect")
 def disconnect():
     session.clear()
-    return redirect("/")
+    return render_template("./index.html")
 
 @app.route("/howtoplay")
 def howtoplay():
@@ -76,18 +70,17 @@ def howtoplay():
 
 @app.route("/profil")
 def profil():
-    if session.get("username") and session.get("id"):
+    if session and session["email"]:
         return render_template("./profil.html", data=get_list_data(session['username']), avatar=get_user_avatar(session["id"]))
     else:
         return redirect("/connexion")
+
 @app.route("/startplaying")
 def startplaying():
     return render_template("./startplaying.html")
 
 @app.route("/createclassic", methods=['GET', 'POST'])
 def createclassic():
-    """"""
-    """"""
     try:
         if not verifie_connexion():
             return redirect("./connexion")
@@ -95,11 +88,10 @@ def createclassic():
             username = session['username']
             gamename = request.form['name']
             maxplayer = int(request.form['maxplayer'])
-            global lastid
-            new_game = Partie(lastid+1, random.randint(0, len(liste_themes)), maxplayer)
-            lastid+=1
-            rooms[new_game.id] = new_game
-            return redirect("./classic_games")
+            if creer_partie_classique(gamename, username, maxplayer):
+                return redirect("./classic_games")
+            else:
+                return "<p> Erreur lors de la création de la partie : veuillez verifier les paramètres </p>"
         else:
             return render_template("./createclassic.html")
     except Exception as e:
@@ -109,13 +101,10 @@ def createclassic():
 @app.route("/classic_games", methods=['GET', 'POST'])
 def classic_games():
     liste_parties = get_classic_games()
-    return render_template("./classic_games.html", gamelist=rooms.values(), themes=liste_themes)
+    return render_template("./classic_games.html", gamelist=liste_parties, iter_liste=range(len(liste_parties)))
 
 @app.route("/classic_queue", methods=['GET', 'POST'])
 def classic_queue():
-    """
-    Afficher la page permettant de lancer une partie
-    """
     if not verifie_connexion():
             return redirect("./connexion")
     room = session.get("room")
@@ -127,12 +116,8 @@ def classic_queue():
 
 @app.route("/check_game")
 def check_game():
-    """
-    Envoie les données d'une partie dès qu'elle est trouvée au js de la page classicqueue
-    """
+    """Envoie les données d'une partie dès qu'elle est trouvée au js de la page classicqueue"""
 
-    return jsonify({"found": True, "status": "En attente", "gameid":10000, "ready":True})
-    #Il aurait fallu refaire le systeme de matchmaking en l'adaptant aux objets de la classe Partie
     #choisir une partie
     found = False
     ready = False
@@ -148,18 +133,31 @@ def check_game():
     if status == "ready":
         ready = True
 
-    rooms[gameid] = Partie(gameid, random.randint(0, len(liste_themes)-1), 6)
+    rooms[gameid] = {"members":0, "messages":[]}
 
     print(f"Found : {found}\nGameid:{gameid}\n Status : {status}\n Max : {maxjoueurs}")
 
     return jsonify({"found": found, "status": status, "gameid":gameid, "ready":ready})
 
 
+@app.route("/ajouter_queue", methods=['GET'])
+def ajouter_queue():
+    gameid = request.args.get("gameid")
+    add_queue(session["id"], gameid)
+    return jsonify({"added":True})
+
+@app.route("/wait_game", methods=['GET']) 
+def wait_game():
+    #Récuperer le gameid passé en paramètre de la requête
+    gameid = request.args.get("gameid")
+    #Tenter de lancer la partie
+    res=attendre_partie(gameid)
+    #Renvoyer True si la partie a bien été lancée, False sinon
+    return jsonify({"ready":res})
+
+
 @app.route("/create", methods=['GET', 'POST'])
 def create():
-    """
-    Endpoint permettant de créer un compte
-    """
     if request.method == 'POST':
         username = request.form.get('username')
         email = request.form.get('email')
@@ -174,34 +172,12 @@ def create():
 
 @app.route("/game/<int:gameid>")
 def show_game(gameid):
-    """
-    Endpoint permettant d'afficher la partie en cours
-    """
     session["room"] = gameid
-    try:
-        return render_template("./ingame.html", room=gameid, partie=rooms[gameid], equipe1=rooms[gameid].equipes["team1"], equipe2=rooms[gameid].equipes["team2"], themes=liste_themes, sessionid=session.get("id"))
-    except:
-        session["room"] = None
-        return "Partie non trouvée"
+    return render_template("./ingame.html", room=gameid)
 
-@app.route("/game/<int:gameid>/game_review")
-def game_review(gameid):
-    """
-    Afficher le résultat d'une partie sur une nouvelle page (les meilleurs messages et leur score)
-    """
-    try:
-        if(rooms[gameid].status != "Terminée"):
-            return redirect(f"/game/{gameid}")
-        return render_template("./game_review.html", gameid=gameid, game_object=rooms[gameid], messages=rooms[gameid].messages)
-    except:
-        return "Partie non trouvée"
-    
 
 @app.route("/edit_profile", methods=['GET', 'POST'])
 def edit_profile():
-    """
-    Gérer la modification de profil, envoi et réception d'un formulaire avec un fichier image
-    """
     if session and session['id']:
         if request.method == 'POST':
             if 'file' not in request.files:
@@ -214,9 +190,15 @@ def edit_profile():
             if file and allowed_file(file.filename):
                 filename = f"{session['id']}.jpg"
                 file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-                set_user_avatar(session["id"], session["id"])
+                con = sqlite3.connect("./database/database.db")
+                cur = con.cursor()
+                sqlreq = f"UPDATE users SET avatar = {session['id']} WHERE id = {session['id']}"
+                cur.execute(sqlreq)
+                con.commit()
+                con.close()
+
                 return redirect("/profil")
-        return render_template("./edit_profile.html", userid=session['id'],  avatar=get_user_avatar(session["id"]))
+        return render_template("./edit_profile.html", userid=session['id'])
     else:
         flash("Erreur")
         return redirect("/")
@@ -225,150 +207,28 @@ def edit_profile():
 
 @socketio.on('connect')
 def handle_connect():
-    """
-    Cette fonction permet de gérer l'ajout d'un utilisateur dans une partie 
-    """
     room = session.get("room")
-    id = session.get("id")
     name = session.get("username")
     if not room or not name:
         return
-    user = Player(id, name)
-    partie = rooms[room]
-    #Verifier si le nombre de JOUEURS max est atteint (place en joueur ou spectateur)
-    if(partie.nb_joueurs == partie.max):
-        pass
-    else:
-        partie.add(user)
-
     join_room(room)
     print(f"{name} a rejoint la room {room}")
 
-@socketio.on('like')
-def handle_like(payload):
-    """
-    Cette fonction permet de gérer les likes côté objet
-    """
-    room = session.get("room")
-    message_id = payload["message_id"]
-    id = session.get("id")
-    name = session.get("username")
-    if not room or not name:
-        return
-    partie = rooms[room]
-    for msg in partie.messages:
-        if msg.id == message_id:
-            msg.likes += 1
-
 @socketio.on('message')
 def handler_message(payload):
-    """
-    Cette fonction permet de gérer l'envoi d'un message et de construire le graphe.
-    """
-
-    #Récupérer les données de la partie
-    rep = payload["rep"]
     room = session.get("room")
-    pid = session.get("id")
     name = session.get("username")
-
-    partie = rooms[room]
-    joueur = partie.get_player(pid)
-    joueur.avatar = get_user_avatar(pid)
-
     if room not in rooms.keys():
         return
     
     message = {
-        "event":"message",
-        "player_id" : pid,
         "sender" : name,
-        "message" : payload["message"],
-        "rep": rep
+        "message" : payload["message"]
     }
-    #Construire un objet message qu'on ajoute à l'objet de la Partie
-    message_object = Message(partie.new_message_id(), joueur, message["message"], time.time())
-    partie.messages.append(message_object)
-
-    #Ajouter quelques informations à envoyer au JS
-    message["date"] = message_object.date
-    message["id"] = message_object.id
-    message["avatar"] = joueur.avatar
-    if joueur in partie.equipes["team1"]:
-        message["equipe"] = 1
-    else:
-        message["equipe"] = 2
-    
-
-    #Envoyer le message à tous les autres joueurs
     send(message, to=room)
+    rooms[room]["messages"].append(message)
 
-    #Ajouter au graphe si besoin
-    if rep is not None:
-        #Si on a séléctionné un message à répondre
-        #ajouter l'objet message à la liste des attaquants du message rep
-        for msg in partie.messages:
-            if msg.id == rep:
-                msg.attaquants.append(message_object)
 
-@socketio.on('update')
-def updateGameInfo(payload):
-    """
-    Mettre a jour les données de la partie 
-    """
-    room = session.get("room")
-    game = rooms[room]
-    if room not in rooms.keys():
-        return
-
-    #changer le choix du joueur    
-    id = game.choose_player()
-    #Recupérer l'utilisateur avec son id dans la partie
-    player = "None"
-    for p in game.players:
-        if p.id == id:
-            player = p.nom
-    if player == "None":
-        return 
-    else:
-        game.talking_time = 80
-        game.round = game.round
-        game.status = "En cours"
-        
-    if(game.round > 3):
-        #Fin de la partie, calcul des scores et ajout des points
-        infos = {
-        "event": "end",
-        "gameid": game.id}
-        print("fin de la partie : ", game.id)
-        for message in game.messages:
-            message.calcul_score()
-        game.sort_scores()
-        send(infos, to=room)
-        if game.status != "Terminée":
-            for message in game.messages:
-            #Calcul du gain (à revoir)
-                gain = message.score
-                if gain > 0:
-                    add_score(message.author, gain)
-        game.status = "Terminée"
-        return infos
-
-    #Mise a jour des equipes
-    equipe1 = [p.nom for p in game.equipes["team1"]]
-    equipe2 = [p.nom for p in game.equipes["team2"]]
-    #Mise a jour de toutes les infos
-    infos = {
-        "event": "update",
-        "status" : game.status,
-        "talking" : player,
-        "id" : id,
-        "equipe1": equipe1,
-        "equipe2": equipe2,
-        "timer" : (int)(round(game.talking_time - game.current_timer + 1, 0)),
-        "round" : game.round
-    }
-    send(infos, to=room)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, allow_unsafe_werkzeug=True)   
--- partie.py
+++ partie.py
@@ -1,166 +1,21 @@
-import time
-import random
-class Player:
-
-    def __init__(self, id, nom):
-        self.id = id
-        self.nom = nom
-        self.messages = []
-        self.avatar = "default"
 
 
 class Partie:
 
-    def __init__(self, id:int, id_thematique:int, max_joueurs:int):
+    def __init__(self, id:int, id_thematique:int, nb_joueurs:int):
         self.id = id
         self.thematiqueID = id_thematique
         self.status = "en attente"
-        self.nb_joueurs = 0
-        self.max = max_joueurs
-        self.players = []
+        self.nb_joueurs = nb_joueurs
         self.equipes = {
             "team1": [],
             "team2": []
         }
-        self.messages = []
-        self.last_message_id = 0
-
-
-        self.round = 1
+        self.msg_hist = []
 
 
 
-        #Ordre de passsage
-        self.talking_time = 10
-        self.talking = 0
-        self.order_index = 0
-        self.order = [0, 3, 1, 4, 2, 5]
-        self.skip = False
-        self.start = time.time()
-        self.change_time = self.start
-        self.current_timer = 0
+class Player:
 
-
-    def player_in(self, player:Player):
-        for p in self.players:
-            if p.id == player.id:
-                return True
-        return False
-
-
-    def add(self, player:Player, team:int=None):
-        """ 
-        
-        Cette methode ajoute une instance d'un joueur dans la partie
-
-        Args:
-            player (Player): instance du joueur de la classe Player
-            team (int): numero de l'equipe 1 ou 2
-
-        Renvoie:
-            true si le joueur a bien été ajouté a la partie
-            false sinon
-        """
-
-        try:
-            if self.player_in(player):
-                return False
-            
-            if team is None:
-                if len(self.equipes["team1"]) == len(self.equipes["team2"]) and len(self.equipes["team1"])  < 3:
-                    self.equipes["team1"].append(player)
-                    self.players.append(player)
-                    self.nb_joueurs += 1
-                    return True
-                elif len(self.equipes["team1"]) == len(self.equipes["team2"]) and len(self.equipes["team1"])  >= 3:
-                    return False
-                elif len(self.equipes["team1"]) > len(self.equipes["team2"]):
-                    self.equipes["team2"].append(player)
-                    self.players.append(player)
-                    self.nb_joueurs += 1
-                    return True
-                return False
-            else:
-                if len(self.equipes[f"team{team}"]) < 3:
-                    self.equipes[f"team{team}"].append(player)
-                    self.players.append(player)
-                    self.nb_joueurs += 1
-                    return True
-        except Exception as e:
-            print("Erreur dans Partie.add() : ", e)
-            return False
-
-    def get_player(self, player_id:int):
-        try:
-            for player in self.players:
-                if player.id == player_id:
-                    return player
-            return None
-        except Exception as e:
-            print("Erreur dans Partie.get_player() : ", e)
-
-    def new_message_id(self):
-        self.last_message_id += 1
-        return self.last_message_id
-
-    def next_talking(self):
-        try:
-            print("Index : ", self.order_index)
-            if self.order_index < self.nb_joueurs:
-                player = self.players[self.order_index]
-                self.order_index += 1
-                return player
-            else:
-                print("new round")
-                self.round += 1
-                self.order_index = 1
-                return self.players[0]
-        except Exception as e:
-            print("Erreur dans next_talking : ", e)
-            return None
-
-    def choose_player(self):
-        self.current_timer = time.time() - self.change_time
-        if(self.talking is None):
-            player = self.next_talking()
-            self.talking = player.id
-            return self.talking
-        if(self.current_timer > self.talking_time or self.skip):
-            self.skip = False
-            #il faut changer de joueur
-            player = self.next_talking()
-            self.change_time = time.time()
-            if(player):
-                self.talking = player.id
-            else:
-                return self.talking
-        return self.talking
-
-    def sort_scores(self):
-        if self.messages == []:
-            return
-        for i in range(0, len(self.messages)):
-            for j in range(0, len(self.messages)):
-                if self.messages[i].score > self.messages[j].score:
-                    self.messages[i], self.messages[j] = self.messages[j], self.messages[i]
-        
-
-
-class Message:
-
-    def __init__(self, id:int, author:Player, content:str, date:int):
-        self.id = id
-        self.author = author
-        self.content = content
-        self.date = date
-        self.score = 0
-        self.likes = 0
-        self.attaquants = []
-
-    
-    def calcul_score(self):
-        gain = self.likes*50 - len(self.attaquants)*50
-        if gain > 0:
-            self.score = gain
-        else:
-            self.score = 0
+    def __init__(self):
+        pass
